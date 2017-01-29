001395cec34b2f0dfbeb53adcf844f317319854f
Nassos Antoniou <nassos@thoughtworks.com>
2017-01-29 16:30:48 +0000

    Implements tracker: a module that monitors which Urls have been crawled

---
+++ track/track.go
@@ -0,0 +1,45 @@
+package track
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/antoniou/go-crawler/fetch"
+	"github.com/antoniou/go-crawler/parse"
+	"github.com/willf/bloom"
+)
+
+type Tracker interface {
+	Run(parser parse.Parser, fetcher fetch.Fetcher) error
+}
+
+type AsynchHttpTracker struct {
+	filter *bloom.BloomFilter
+}
+
+func New() *AsynchHttpTracker {
+	filter := bloom.New(20000, 5)
+	t := &AsynchHttpTracker{
+		filter: filter,
+	}
+	return t
+}
+
+func (t *AsynchHttpTracker) Run(parser parse.Parser, fetcher fetch.Fetcher) error {
+	fmt.Println("Tracker: Starting...")
+	for {
+		fmt.Println("Tracker: Waiting for input from Parser")
+		res, _ := parser.Retrieve()
+		if !t.filter.TestAndAddString(res) {
+			url, err := url.ParseRequestURI(res)
+			if err != nil {
+				return err
+			}
+			fmt.Printf("Tracker: Requesting to fetch %s from Fetcher\n", url)
+			go fetcher.Fetch(url)
+		} else {
+			fmt.Printf("Tracker: Url %s is already in bloom filter\n", res)
+		}
+	}
+
+}
