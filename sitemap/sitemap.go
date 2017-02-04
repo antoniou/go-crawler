7bcbd4c5bb3a68b883273a8d115a2475ffdcda06
Nassos Antoniou <nassos@thoughtworks.com>
2017-02-04 19:06:25 +0000

    Refactor sitemapper

--- sitemap/sitemap.go
+++ sitemap/sitemap.go
@@ -1,15 +1,17 @@
 package sitemap
 
-import (
-	"fmt"
-
-	"github.com/twmb/algoimpl/go/graph"
-)
+import "github.com/twmb/algoimpl/go/graph"
 
 type Sitemapper interface {
 	// Add creates a representation of the
 	// quad to the Sitemapper
 	Add(from string, to string) error
+
+	//SeedURL returns the seed URL of the Sitemap
+	SeedURL() string
+
+	//LinksFrom returns the links from a specific node
+	LinksFrom(URL string) *[]string
 }
 
 type GraphSitemap struct {
@@ -27,25 +29,50 @@ func New() *GraphSitemap {
 	}
 }
 
-func (s *GraphSitemap) Add(from string, to string) error {
-	fmt.Printf("1.Adding node for %s\n", to)
+func (s *GraphSitemap) addRoot(root string) {
+	node := s.addNode(root)
+	s.hasNodes = true
+	s.root = &node
+}
+
+func (s *GraphSitemap) addNode(nodeURL string) graph.Node {
 	node := s.graph.MakeNode()
-	*node.Value = to
-	s.nodemap[to] = &node
+	*node.Value = nodeURL
+	s.nodemap[nodeURL] = &node
+	return node
+}
 
-	_, ok := s.nodemap[from]
-	if !ok {
-		fmt.Printf("1.Adding node for %s\n", from)
-		nodeFrom := s.graph.MakeNode()
-		*nodeFrom.Value = from
-		s.nodemap[from] = &nodeFrom
-		if !s.hasNodes {
-			fmt.Printf("Root node is %s\n", from)
-			s.hasNodes = true
-			s.root = &nodeFrom
-		}
+func (s *GraphSitemap) Add(from string, to string) error {
+	if !s.hasNodes {
+		s.addRoot(to)
+		return nil
 	}
-	s.graph.MakeEdge(*s.nodemap[from], node)
 
+	nodeTo := s.addNode(to)
+
+	var nodeFrom *graph.Node
+	nodeFrom, ok := s.nodemap[from]
+	if !ok {
+		nodef := s.addNode(from)
+		nodeFrom = &nodef
+	}
+
+	s.graph.MakeEdge(*nodeFrom, nodeTo)
 	return nil
+}
+
+//SeedURL Returns the seed URL (Root) of the Sitemap
+func (s *GraphSitemap) SeedURL() string {
+	return (*s.root.Value).(string)
+}
+
+//LinksFrom returns the links from a specific node
+// as an unprioritised String slice
+func (s *GraphSitemap) LinksFrom(url string) *[]string {
+	links := make([]string, 0, 100)
+	for _, node := range s.graph.Neighbors(*s.nodemap[url]) {
+		val := (*node.Value).(string)
+		links = append(links, val)
+	}
+	return &links
 }
