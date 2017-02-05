9713336ca7d9873a93be9142879a53403d8cce3f
Nassos Antoniou <nassos@thoughtworks.com>
2017-02-05 15:30:04 +0000

    Removes subcommand style command line client

--- client/client.go
+++ client/client.go
@@ -1,67 +1,100 @@
 package client
 
 import (
+	"fmt"
 	"log"
+	"net/url"
+	"os"
 
+	"github.com/antoniou/go-crawler/crawl"
+	"github.com/antoniou/go-crawler/sitemap"
+	"github.com/antoniou/go-crawler/util"
+	"github.com/goware/urlx"
 	"github.com/urfave/cli"
 )
 
-//Client represents a Mistri command line client
+//Client represents a command line client
 type Client struct {
-	app      *cli.App
-	commands map[string]Command
+	app *cli.App
 }
 
-func (c *Client) RegisterCommand(name string, cmd Command) error {
-	_, registered := c.commands[name]
-	if registered {
-		return clientError{s: "Command %s already registered"}
-	}
-	c.commands[name] = cmd
-	return nil
+func (client *Client) Run(arguments []string) error {
+	return client.app.Run(arguments)
 }
 
-func (c *Client) Run(arguments []string) error {
-	return c.app.Run(arguments)
-}
-
-func (c *Client) RegisterCommands() error {
-	if c.commands == nil {
-		c.commands = make(map[string]Command)
-	}
-	err := c.RegisterCommand("crawl", NewCrawlCommand())
-	if err != nil {
-		log.Print(err)
-	}
-	return err
-}
-
-func New() (c *Client) {
-	c = &Client{}
-	c.RegisterCommands()
-
+func New() (client *Client) {
+	client = new(Client)
 	app := cli.NewApp()
 	app.Name = "go-crawler"
-	app.Usage = ""
-	app.Commands = []cli.Command{}
-	for name, cmd := range c.commands {
-		app.Commands = append(app.Commands, cli.Command{
-			Name: name,
-			Action: func(c *cli.Context) error {
-				return cmd.Run(c.Args())
-			},
-		})
+	app.Usage = "Crawl a site"
+	app.UsageText = "crawl [options] url"
+	app.Version = "0.1.0"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "o",
+			Value: "result.out",
+			Usage: "Output file",
+		},
+		cli.BoolFlag{
+			Name:  "verbose",
+			Usage: "Verbose mode",
+		},
 	}
-	c.app = app
-	return c
+
+	app.Action = func(c *cli.Context) error {
+		return client.Crawl(c)
+	}
+
+	client.app = app
+	return client
 }
 
-// clientError is an error used to signal different error situations in command handling.
-type clientError struct {
-	s         string
-	userError bool
-}
+// Crawl initiates the crawling steps
+// Requires one or more valid url strings
+func (client *Client) Crawl(c *cli.Context) error {
+	args := c.Args()
 
-func (c clientError) Error() string {
-	return c.s
+	if len(args) == 0 {
+		return fmt.Errorf("Expects at least one argument")
+	}
+
+	normURL, _ := urlx.NormalizeString(args[0])
+	url, err := url.ParseRequestURI(normURL)
+
+	if err != nil {
+		return err
+	}
+
+	_ = util.Logger(c.Bool("verbose"))
+	fetcher := crawl.NewAsyncHTTPFetcher()
+	parser := crawl.NewAsyncHTTPParser(url, fetcher)
+	tracker := crawl.New(fetcher, parser)
+
+	crawler := crawl.NewAsyncHTTPCrawler(
+		fetcher,
+		tracker,
+		[]crawl.Worker{
+			parser.Worker(),
+		},
+	)
+
+	stmp, err := crawler.Crawl(url)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	outfile := c.String("o")
+	f, err := os.Create(outfile)
+	if err != nil {
+		return err
+	}
+
+	err = sitemap.NewExporter(f).Export(stmp)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Sitemap exported to %s\n", outfile)
+	return err
 }
